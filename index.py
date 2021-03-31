dc17f125428eda1902bd2e641b1ff519370138e0
vietnamican <vietnamican@gmail.com>
2021-03-31 11:05:08 +0700

    replace bn by identity

--- index.py
+++ index.py
@@ -1,3 +1,5 @@
+from time import time
+
 import torch
 from torch import nn
 from torchsummary import summary
@@ -10,5 +12,17 @@ if __name__ == '__main__':
     # model.eval()
     # state_dict = torch.load('checkpoints/resnet34_state.ckpt')
     # model.load_state_dict(state_dict)
+    # summary(model, (3, 32, 32), depth=6)
+    x = torch.Tensor(1, 3, 32, 32)
+    start = time()
+    for i in range(200):
+        model(x)
+    stop = time()
+    print(stop - start)
     merge(model)
-    # summary(model, (3, 32, 32))
+    start = time()
+    for i in range(200):
+        model(x)
+    stop = time()
+    print(stop - start)
+    # summary(model, (3, 32, 32), depth=6)
